201a518c9b4608a4b79471e049c772d7e1006974
indexer <yemonkyaw007@gmail.com>
2017-11-20 21:54:03 +0800

    update on fun insert

--- app/src/main/java/com/indexer/ccoin/viewmodel/CoinListViewModel.kt
+++ app/src/main/java/com/indexer/ccoin/viewmodel/CoinListViewModel.kt
@@ -7,24 +7,40 @@ import android.arch.lifecycle.LiveData
 import android.util.Log
 import com.indexer.ccoin.api.RestClient
 import com.indexer.ccoin.database.AppDatabase
+import com.indexer.ccoin.model.Coin
 import com.indexer.ccoin.utils.enqueue
+import io.reactivex.android.schedulers.AndroidSchedulers
+import java.util.ArrayList
 
 
 class CoinListViewModel(application: Application) : AndroidViewModel(application) {
 
-    var mAppDatabase: AppDatabase = AppDatabase.getDatabase(application.applicationContext)
+    private var mAppDatabase: AppDatabase = AppDatabase
+            .getDatabase(application.applicationContext)
+
+    private var mList = ArrayList<Coin>()
+
 
     fun isDataBaseNotCreate(): LiveData<Boolean> {
         return mAppDatabase.isDatabaseCreated
+    }
+
+    fun insertData(coins: List<Coin>) {
+        io.reactivex.Observable.fromCallable<List<Coin>> {
+            mAppDatabase.coinDao.insertAllCoin(coins)
+            null
+        }.observeOn(AndroidSchedulers.mainThread()).subscribe()
     }
 
     fun fetchDataFromCurrencyCompare() {
         val coinList = RestClient.getService(getApplication())
                 .getCoinList()
         coinList.enqueue(success = { response ->
-            Log.e("isResponse", response.message())
             if (response.isSuccessful) {
-
+                for ((_, value) in response.body()?.data!!.entries) {
+                    mList.add(value)
+                }
+                insertData(mList)
             }
 
         }, failure = { response ->
